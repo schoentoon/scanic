d751be2fe2ee29a2e7f0ab3abaaef830693d1a78
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-06-22 17:57:27 +0200

    Let's correctly keep track of actions
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- include/message.h
+++ include/message.h
@@ -19,6 +19,11 @@ private:
    * The sender of the message
    */
   std::string _author;
+
+  /**
+   * Is this message actually an action?
+   */
+  bool _action = false;
 public:
   const std::tm time() const { return _time; };
   Message& time(std::tm time) {
@@ -35,6 +40,12 @@ public:
   const std::string author() const { return _author; };
   Message& author(std::string author) {
     _author = author;
+    return *this;
+  };
+
+  const bool action() const { return _action; };
+  Message& action(bool action) {
+    _action = action;
     return *this;
   };
 };
--- src/input_znc.cpp
+++ src/input_znc.cpp
@@ -15,11 +15,23 @@ Message ZncInput::process(const std::string& input, const std::string &path) con
 
   std::size_t open_user = input.find_first_of('<');
   std::size_t end_user = input.find_first_of('>');
-  if (open_user == std::string::npos || end_user == std::string::npos) throw std::runtime_error("Couldn't find username..");
-  output.author(input.substr(open_user + 1, end_user - open_user - 1)); // +1 to the open to skip the < and - 1 to the end to skip the >
+  if (open_user == std::string::npos || end_user == std::string::npos) {
+    // For now we simply skip any form of actions
+    if (input.find("***") != std::string::npos) throw std::runtime_error("Couldn't find username..");
+
+    // Search for the "* " which indicates an action
+    open_user = input.find("* ");
+    if (open_user == std::string::npos) throw std::runtime_error("Couldn't find username..");
+    open_user += 2; // Increase it by 2 so we're at the first character of the username
+
+    end_user = input.find_first_of(' ', open_user);
+    output.action(true) // mark the message as an action
+          .author(input.substr(open_user, end_user - open_user));
+  } else
+    output.author(input.substr(open_user + 1, end_user - open_user - 1)); // +1 to the open to skip the < and - 1 to the end to skip the >
 
   // + 2 to skip the > of the username and the space just after it
   output.message(input.substr(end_user + 2));
 
-  return Message();
+  return output;
 }
