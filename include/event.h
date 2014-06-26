565deea58594035f3a2b06e0a047795fe657498a
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-06-26 21:35:36 +0200

    Let's just store the time of the events as a std::time_t, uses slightly less memory
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- include/event.h
+++ include/event.h
@@ -8,16 +8,23 @@ private:
   /**
    * The timestamp of the message
    */
-  std::tm _time;
+  std::time_t _time;
 public:
   Event(std::tm &&time)
+  : _time(std::mktime(&time)) {
+  };
+
+  Event(std::time_t &&time)
   : _time(std::move(time)) {
   };
 
-  Event() {};
+  Event() : _time(0) {};
 
-  const std::tm time() const { return _time; };
+  const std::time_t time() const { return _time; };
   void time(std::tm time) {
+    _time = std::mktime(&time);
+  };
+  void time(std::time_t time) {
     _time = time;
   };
 };
